c14fc2d9994bf02c1dc6e60ba799e047ca63ed6d
Juan F <jfranciscomgz@gmail.com>
2026-04-22 18:51:17 -0400

    Add Static pages!

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,11 +1,14 @@
 # Uncomment the imports before you add the code
-# from django.urls import path
+from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-# from . import views
+from . import views
+from django.views.generic import TemplateView
 
 app_name = 'djangoapp'
 urlpatterns = [
+    
+
     # # path for registration
 
     # path for login

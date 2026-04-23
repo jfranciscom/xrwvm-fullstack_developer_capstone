5bf8039d47fe55135c12a13c506bbf539e563849
Juan F <jfranciscomgz@gmail.com>
2026-04-23 09:32:30 -0400

    Module 3

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path(route='login', view=views.login_user, name='login'),
     path(route='logout', view=views.logout_request, name='logout_request'),
     # path for dealer reviews view
-
+    path(route='get_cars', view=views.get_cars, name="get_cars"),
     # path for add a review view
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
